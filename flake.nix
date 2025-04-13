b9373dd86890fda4dedeb5d6e715a0adc6b4ddad
Aljebriq <143266740+aljebriq@users.noreply.github.com>
2025-04-13 21:32:18 +0200

    Syntax rework (#6)

--- flake.nix
+++ flake.nix
@@ -6,18 +6,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShellNoCC {
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
           packages = with pkgs; [
             reuse
             bun
@@ -26,6 +30,6 @@
             nodePackages.npm
           ];
         };
-      }
-    );
+      });
+    };
 }
